97f331dca23e75a47d0f8a38ebb7ec2b9e993a25
Dmitry Luzin <dimanoshe@gmail.com>
2023-03-28 13:22:38 +0400

    Refactor bot_app

--- bot_app/tests/test_bot.py
+++ bot_app/tests/test_bot.py
@@ -1,24 +1,7 @@
 import os
 import pytest
-from bot_app import handle_message
-import telebot
+from ..bot_app import handle_message
 
-
-bot = telebot.TeleBot(os.getenv("TELEGRAM_API_KEY"))
-print(f'token: {os.getenv("TELEGRAM_API_KEY")}')
-@bot.message_handler(func=lambda _: True)
-def handle_message(message):
-    return message
-
-
-
-@pytest.mark.parametrize("message, expected", [
-        (handle_message('test'), 'Test Ok!'),
-    ],
-    ids=[
-        'first test',
-    ]
-)
 
 def test_bot_app(message, expected):
     assert handle_message(message) == expected
